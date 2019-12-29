a81ccf427bac5f1c559764fff8e983e7067f7ad8
J. B. Rainsberger <me@jbrains.ca>
2019-12-29 14:21:25 -0500

    Documented how to parse TypedTime strings of the form 00:xx representing less than 1 minute.

--- tests/LearnTypedTime.elm
+++ tests/LearnTypedTime.elm
@@ -9,7 +9,33 @@ import TypedTime exposing (Unit(..), fromString, seconds)
 
 allTests : Test
 allTests =
-    failingTestsDueToParsingIssue
+    Test.concat [ parseTimeUnderOneMinuteInSeconds, failingTestsDueToParsingIssue ]
+
+
+parseTimeUnderOneMinuteInSeconds : Test
+parseTimeUnderOneMinuteInSeconds =
+    let
+        correctlyFormatTimeUnderOneMinuteInSeconds n =
+            "00:" ++ String.padLeft 2 '0' (Debug.toString n)
+    in
+    fuzz (intRange 0 59) "We must format times under 1 minute as 00:xx due to https://github.com/jxxcarlson/elm-typed-time/issues/1" <|
+        \secondsAsInt ->
+            case secondsAsInt of
+                0 ->
+                    -- This case works by accident, so don't bother checking it
+                    Expect.pass
+
+                _ ->
+                    let
+                        typedTimeInSecondsAsText =
+                            correctlyFormatTimeUnderOneMinuteInSeconds
+
+                        expectedResultInMaybeSeconds =
+                            toFloat >> seconds >> Just
+                    in
+                    typedTimeInSecondsAsText secondsAsInt
+                        |> TypedTime.fromString Seconds
+                        |> Expect.equal (expectedResultInMaybeSeconds secondsAsInt)
 
 
 failingTestsDueToParsingIssue : Test
@@ -23,7 +49,7 @@ failingTestsDueToParsingIssue =
 
                 _ ->
                     let
-                        typedTimeInSecondsAsText n =
+                        incorrectlyButReasonablyTryToFormatTimeUnderOneMinuteInSeconds n =
                             "0:"
                                 ++ (if n >= 10 then
                                         Debug.toString n
@@ -35,7 +61,7 @@ failingTestsDueToParsingIssue =
                         expectedResultInMaybeSeconds =
                             toFloat >> seconds >> Just
                     in
-                    typedTimeInSecondsAsText secondsAsInt
+                    incorrectlyButReasonablyTryToFormatTimeUnderOneMinuteInSeconds secondsAsInt
                         |> TypedTime.fromString Seconds
                         |> Expect.notEqual (expectedResultInMaybeSeconds secondsAsInt)
                         |> Expect.onFail "Maybe this issue is fixed? https://github.com/jxxcarlson/elm-typed-time/issues/1"
