ecfb1e9de6131eba5ca5fbb87dfaf4397ff81685
J. B. Rainsberger <me@jbrains.ca>
2019-12-29 13:38:54 -0500

    We can set the time, but not to less than 1 minute using the 0:xx notation, due to something strange in the TypedTime module.

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,9 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
+import Html exposing (Html, button, div, input, text)
+import Html.Attributes exposing (placeholder, value)
+import Html.Events exposing (onClick, onInput)
 import Timer exposing (Timer)
 import TypedTime exposing (minutes)
 
@@ -12,34 +13,53 @@ main =
 
 
 type alias Model =
-    { timer : Timer }
+    { timer : Timer, setTimeText : String }
 
 
 init : Model
 init =
-    { timer = Timer.activeTimerSetTo (minutes 10) }
+    { timer = Timer.activeTimerSetTo (minutes 10), setTimeText = "" }
 
 
 type Msg
     = Tick Timer
+    | UpdateSetTimeText String
+    | SetRemainingTime Timer String
 
 
 update : Msg -> Model -> Model
-update msg _ =
+update msg model =
     case msg of
         Tick timer ->
-            { timer = Timer.tick timer }
+            { model | timer = Timer.tick timer }
+
+        UpdateSetTimeText text ->
+            { model | setTimeText = text }
+
+        SetRemainingTime timer newTimeAsText ->
+            let
+                maybeNewTime =
+                    TypedTime.fromString TypedTime.Seconds newTimeAsText
+            in
+            case maybeNewTime of
+                Just newTime ->
+                    { model | timer = Timer.activeTimerSetTo newTime }
+
+                Nothing ->
+                    model
 
 
 view : Model -> Html Msg
-view { timer } =
+view { timer, setTimeText } =
     div []
-        [ viewTimer timer ]
+        [ viewTimer timer setTimeText ]
 
 
-viewTimer : Timer -> Html Msg
-viewTimer timer =
+viewTimer : Timer -> String -> Html Msg
+viewTimer timer setTimeText =
     div []
         [ button [ onClick (Tick timer) ] [ text "tick" ]
+        , input [ placeholder "Set time to", value setTimeText, onInput UpdateSetTimeText ] []
+        , button [ onClick (SetRemainingTime timer setTimeText) ] [ text "set" ]
         , div [] [ TypedTime.toString TypedTime.Seconds (Timer.timeRemainingInSeconds timer |> toFloat |> TypedTime.seconds) |> text ]
         ]
