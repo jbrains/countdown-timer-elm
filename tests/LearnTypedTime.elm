3dec546d5f8a4c65a03369685b0c087471e8651e
J. B. Rainsberger <me@jbrains.ca>
2019-12-29 14:07:31 -0500

    Documented an apparent defect in jxxcarlson/elm-typed-time.

---
+++ tests/LearnTypedTime.elm
@@ -0,0 +1,41 @@
+module LearnTypedTime exposing (allTests)
+
+import Expect
+import Fuzz exposing (..)
+import Test exposing (..)
+import Timer exposing (..)
+import TypedTime exposing (Unit(..), fromString, seconds)
+
+
+allTests : Test
+allTests =
+    failingTestsDueToParsingIssue
+
+
+failingTestsDueToParsingIssue : Test
+failingTestsDueToParsingIssue =
+    fuzz (intRange 0 59) "Parsing 0:xx in seconds fails, because the parser expects '00' in place of '0'" <|
+        \secondsAsInt ->
+            case secondsAsInt of
+                0 ->
+                    -- This case works by accident, so don't bother checking it
+                    Expect.pass
+
+                _ ->
+                    let
+                        typedTimeInSecondsAsText n =
+                            "0:"
+                                ++ (if n >= 10 then
+                                        Debug.toString n
+
+                                    else
+                                        "0" ++ Debug.toString n
+                                   )
+
+                        expectedResultInMaybeSeconds =
+                            toFloat >> seconds >> Just
+                    in
+                    typedTimeInSecondsAsText secondsAsInt
+                        |> TypedTime.fromString Seconds
+                        |> Expect.notEqual (expectedResultInMaybeSeconds secondsAsInt)
+                        |> Expect.onFail "Maybe this issue is fixed? https://github.com/jxxcarlson/elm-typed-time/issues/1"
