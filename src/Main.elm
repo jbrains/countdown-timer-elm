3e82894735d5becbd02a0bef005381a3668491ce
J. B. Rainsberger <me@jbrains.ca>
2019-12-29 13:15:57 -0500

    The timer app can tick a single timer.

---
+++ src/Main.elm
@@ -0,0 +1,39 @@
+module Main exposing (..)
+
+import Browser
+import Html exposing (Html, button, div, text)
+import Html.Events exposing (onClick)
+import Timer exposing (Timer)
+import TypedTime exposing (minutes)
+
+
+main =
+    Browser.sandbox { init = init, update = update, view = view }
+
+
+type alias Model =
+    { timer : Timer }
+
+
+init : Model
+init =
+    { timer = Timer.activeTimerSetTo (minutes 10) }
+
+
+type Msg
+    = Tick
+
+
+update : Msg -> Model -> Model
+update msg { timer } =
+    case msg of
+        Tick ->
+            { timer = Timer.tick timer }
+
+
+view : Model -> Html Msg
+view { timer } =
+    div []
+        [ button [ onClick Tick ] [ text "tick" ]
+        , div [] [ TypedTime.toString TypedTime.Seconds (Timer.timeRemainingInSeconds timer |> toFloat |> TypedTime.seconds) |> text ]
+        ]
