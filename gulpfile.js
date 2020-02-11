e2c8a221f9d9a2faded4cf34265e64c893c9149e
J. B. Rainsberger <me@jbrains.ca>
2020-02-11 13:11:30 -0400

    Declared the default gulp tasks by intention.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,23 @@
-const defaultTask = async function() {
-  console.log("Hello, world. Gulp is running.");
-  await Promise.resolve({});
+const { series } = require("gulp");
+
+const clean = async function() {
+  console.log("Clean: remove generated code.");
+  await Promise.resolve({succeeded: true});
 }
 
-exports.default = defaultTask;
+const runServer = async function() {
+  console.log("Run the server on port 4001.");
+  await Promise.resolve({succeeded: true});
+}
+
+const watchElmCode = async function() {
+  console.log("Watch the Elm code, to rebuild it as it changes.");
+  await Promise.resolve({succeeded: true});
+}
+
+const buildElmCode = async function() {
+  console.log("Build the Elm code.");
+  await Promise.resolve({succeeded: true});
+}
+
+exports.default = series(clean, runServer, watchElmCode, buildElmCode);
