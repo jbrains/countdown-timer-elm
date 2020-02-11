c621c689ad6f83298cc6a6b752f2961c0b74ca3e
J. B. Rainsberger <me@jbrains.ca>
2020-02-11 13:33:43 -0400

    We can now clean and build the Elm code.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
-const { series } = require("gulp");
+const { series, watch, src, dest } = require("gulp");
+const elm = require("gulp-elm");
+const del = require("del");
 
 const clean = async function() {
   console.log("Clean: remove generated code.");
+  del.sync(["elm/target", "jekyll/source/javascripts/countdown-timer-elm.js"]);
   await Promise.resolve({succeeded: true});
 }
 
@@ -12,12 +15,18 @@ const runServer = async function() {
 
 const watchElmCode = async function() {
   console.log("Watch the Elm code, to rebuild it as it changes.");
+  // watch("elm/src/**/*.elm", series(buildElmCode));
   await Promise.resolve({succeeded: true});
 }
 
 const buildElmCode = async function() {
   console.log("Build the Elm code.");
+  src("elm/src/Main.elm")
+     .pipe(elm.bundle("countdown-timer-elm.js", { optimize: true, cwd: "elm" }))
+     .pipe(dest("jekyll/source/javascripts"));
   await Promise.resolve({succeeded: true});
 }
 
+exports.build = buildElmCode;
+exports.clean = clean;
 exports.default = series(clean, runServer, watchElmCode, buildElmCode);
